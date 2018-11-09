ef9338adc196a9d9f22caca338751ff95e9ed1f9
nut_tharin <37585893+nuttharin@users.noreply.github.com>
2018-11-09 14:35:00 +0700

    frist

---
+++ api/controllers/webService.Controllers.js
@@ -0,0 +1,23 @@
+const MongoClient = require('mongodb').MongoClient
+
+const url = "mongodb://localhost:27017/api/"
+
+exports.get_all = (req, res, next) =>{
+    MongoClient.connect(url,function(err,db){
+        db.collection("Weather3Hours")
+        .find({})
+        .toArray(function(err ,result)
+        {
+            if(err) throw err 
+            const output =
+            {
+                status:"OK",
+                message  : result
+            }
+            res.json(output)
+            
+             db.close()
+        })
+    })
+
+}
